cff32d5330da4c8e0a9cf5656499f7a91963a1f1
ruiponte1990 <ruiponte1990@gmail.com>
2020-11-23 22:12:12 -0500

    aops

---
+++ brackets/tests/brackets_test.py
@@ -0,0 +1,47 @@
+from src.brackets import *
+#from brackets_canon import solution
+# uncomment or comment to switch between solutions
+
+def test_simple():
+    assert solution("{}") == True
+    assert solution("()") == True
+    assert solution("[]") == True
+
+def test_onion():
+    assert solution("({[]})") == True
+    assert solution("[({})]") == True
+    assert solution("{[()]}") == True
+    assert solution("[{[()]}]") == True
+    assert solution("(([{[()]}]))") == True
+
+def test_simple_fail():
+    assert solution("}") == False
+    assert solution("[") == False
+    assert solution("[}") == False
+    assert solution("(}") == False
+    assert solution("[)") == False
+
+def test_trailing():
+    assert solution("({[]})]") == False
+    assert solution("({[]})}") == False
+    assert solution("{({[]})") == False
+    assert solution("]({[]})") == False
+
+def test_multi_children():
+    assert solution("[{}{}]") == True
+    assert solution("[{}()]") == True
+    assert solution("[{}(]]") == False
+    assert solution("[{}{[]]") == False
+    assert solution("[{}[()]{}]") == True
+    assert solution("[{}[()]{}{}[]()]") == True
+
+def test_single_level():
+    assert solution("{}[]") == True
+    assert solution("{}[][]{}[]") == True
+    assert solution("{}(]") == False
+
+def test_single_level_with_children():
+    assert solution("{[]}[()]") == True
+    assert solution("{[]}[()]{}") == True
+    assert solution("{[]}[()]{}") == True
+    assert solution("{[]}{()]") == False
